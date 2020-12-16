32b10070faa1ee4a4a6fd3f5164c84b449e054fb
Charly <brianrecinosc@hotmail.com>
2020-12-15 22:00:03 -0500

    Data Annotations, routing and linkgenerator for controller.

--- Controllers/CampsController.cs
+++ Controllers/CampsController.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Routing;
 
 namespace CoreCodeCamp.Controllers
 {
@@ -18,11 +19,13 @@ namespace CoreCodeCamp.Controllers
     {
         private readonly ICampRepository _repository;
         private readonly IMapper _mapper;
+        private readonly LinkGenerator _linkGenerator;
 
-        public CampsController(ICampRepository repository, IMapper  mapper)
+        public CampsController(ICampRepository repository, IMapper  mapper, LinkGenerator  linkGenerator)
         {
             _repository = repository;
             _mapper = mapper;
+            _linkGenerator = linkGenerator;
         }
 
         [HttpGet]
@@ -64,22 +67,84 @@ namespace CoreCodeCamp.Controllers
             }
         }
 
+        [HttpGet("{moniker}")]
+        public async Task<ActionResult<CampModel[]>> Get(string moniker)
+        {
+            try
+            {
+                var results = await _repository.GetCampAsync(moniker);
+                if (results == null)
+                {
+                    return NotFound();
+                }
+
+                return _mapper.Map<CampModel[]>(results);
+            }
+            catch (Exception)
+            {
+
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Error- Get moniker error");
+            }
+        }
 
         public async Task<ActionResult<CampModel>> Post([FromBody] CampModel model)
         {
             try
             {
-                return Ok();
+                var location = _linkGenerator.GetPathByAction("Get", "Camps", new { moniker = model.Moniker });
+
+                if (string.IsNullOrEmpty(location))
+                {
+                    return BadRequest("Could not use current moniker");
+                }
+                
+                var camp = _mapper.Map<Camp>(model);
+                _repository.Add(camp);
+                if (await _repository.SaveChangesAsync())
+                {
+                    return Created( $"/api/camps/{camp.Moniker}" , _mapper.Map<CampModel>(camp));
+                }
+
+                return BadRequest();
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database error, on Post method (CampsController)");
+            }
+        
+        }
+
+        [HttpPut("{moniker}")]
+        public async Task<ActionResult<CampModel>> Put(string moniker, CampModel model)
+        {
+            try
+            {
+                var oldCamp = await _repository.GetCampAsync(moniker);
+                if (oldCamp == null)
+                {
+                    return NotFound($"Could not find camp with moniker of {moniker}");
+                }
+
+                _mapper.Map(model, oldCamp);
+
+                if (await _repository.SaveChangesAsync())
+                {
+                    return _mapper.Map<CampModel>(oldCamp);
+                }
+                else
+                {
+                    return NotFound();
+                }
+
+
             }
             catch (Exception)
             {
 
-                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database error, on Post method (CampsController)");
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database error, on Put method (CampsController)"); ;
             }
         
         
-        
         }
-
     }
 }
